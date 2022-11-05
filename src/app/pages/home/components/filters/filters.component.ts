f91de639f1a34a3b7e7d65d69fef3abf4a82125e
Yash Shah <yashshah1282@gmail.com>
2022-11-05 12:34:22 +0300

    Added filters html file

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './filters.component.html'
 })
 export class FiltersComponent implements OnInit {
-  categories: string[] = ['shoes', 'sports'];
+  categories: string[] = ['Shoes', 'Sports'];
 
   constructor() { }
 
